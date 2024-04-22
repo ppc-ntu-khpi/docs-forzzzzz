ceadd918f3a4073526b39287a2dab2f517508b6d
Mark Tkachenko <mark.marktka@gmail.com>
2024-04-22 18:46:15 +0000

    add comments javadoc

--- src/domain/Artist.java
+++ src/domain/Artist.java
@@ -1,43 +1,86 @@
 package domain;
 
+/**
+ * Клас Artist представляє собою художника, який є працівником. Він розширює клас Employee
+ * і додає функціонал, пов'язаний з навичками художника.
+ */
 public class Artist extends Employee {
 
-    public Artist(String[] skiils, String name, String jobTitle, int level, String dept) {
+    /**
+     * Створює художника із вказаними навичками, ім'ям, посадою, рівнем та відділом.
+     *
+     * @param skills   масив рядків, що представляють навички художника
+     * @param name     ім'я художника
+     * @param jobTitle посада художника
+     * @param level    рівень художника
+     * @param dept     відділ художника
+     */
+    public Artist(String[] skills, String name, String jobTitle, int level, String dept) {
         super(name, jobTitle, level, dept);
-        this.skiils = skiils;
+        this.skills = skills;
     }
 
-    public Artist(String[] skiils) {
+    /**
+     * Створює художника із вказаними навичками.
+     *
+     * @param skills масив рядків, що представляють навички художника
+     */
+    public Artist(String[] skills) {
         super();
-        this.skiils = skiils;
+        this.skills = skills;
     }
     
+    /**
+     * Створює художника із стандартними навичками.
+     */
     public Artist() {
         super();
-        this.skiils = new String[10];
+        this.skills = new String[10];
     }
 
+    /**
+     * Повертає рядкове представлення художника, включаючи його навички.
+     *
+     * @return рядкове представлення художника
+     */
     @Override
     public String toString() {
-        return super.toString()+"\nSkills: "+getSkills();
+        return super.toString() + "\nНавички: " + getSkills();
     }
 
-    private String[] skiils;
+    private String[] skills;
 
+    /**
+     * Повертає навички художника у вигляді рядка.
+     *
+     * @return рядок, що представляє навички художника
+     */
     public String getSkills() {
-        String s = "";
-        for (String e : skiils) {
-            s = s + e + ", ";
+        StringBuilder sb = new StringBuilder();
+        for (String skill : skills) {
+            sb.append(skill).append(", ");
         }
-       s=s.substring(0, s.length() - 2);
-        return s;
+        if (sb.length() > 2) {
+            sb.setLength(sb.length() - 2);
+        }
+        return sb.toString();
     }
 
+    /**
+     * Встановлює навички художника.
+     *
+     * @param skills масив рядків, що представляють навички художника
+     */
     public void setSkills(String[] skills) {
-        this.skiils=skills;
+        this.skills = skills;
     }
 
-    public String[] getSkillsLSist() {
-        return skiils;
+    /**
+     * Повертає навички художника у вигляді масиву.
+     *
+     * @return масив рядків, що представляє навички художника
+     */
+    public String[] getSkillsList() {
+        return skills;
     }
 }
